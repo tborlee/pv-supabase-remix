5eaaa87bc5bfedb4d84a770cac9dd6dbfa1730ba
Thomas Borlée <thomas@borlee.be>
2024-11-25 00:14:18 +0100

    Bugfix

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,15 +1,14 @@
-import type { Config } from "@react-router/dev/config"
+import type { Config } from "@react-router/dev/config";
 
 export default {
-    ignoredRouteFiles: ["**/.*"],
-    serverModuleFormat: "cjs",
-    future: {
-        unstable_optimizeDeps: true,
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_lazyRouteDiscovery: true,
-        v3_singleFetch: true,
-        v3_routeConfig: true,
-    },
-}
+  ignoredRouteFiles: ["**/.*"],
+  future: {
+    unstable_optimizeDeps: true,
+    v3_fetcherPersist: true,
+    v3_relativeSplatPath: true,
+    v3_throwAbortReason: true,
+    v3_lazyRouteDiscovery: true,
+    v3_singleFetch: true,
+    v3_routeConfig: true,
+  },
+};
